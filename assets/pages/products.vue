e65412e95f64ac9c0d5f3422fa72081e7b74889f
Nuxea <lescieux.am@gmail.com>
2024-05-03 11:43:49 +0200

    Single File Component

---
+++ assets/pages/products.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "Products",
+    data() {
+        return {
+            jobName: 'Programming',
+        };
+    },
+};
+</script>
+
+<template>
+    <h1>Hi! I ❤️ {{ jobName }}</h1>
+</template>
+
+<style scoped>
+
+</style>
--- assets/products.js
+++ assets/products.js
@@ -1,17 +1,4 @@
-import {compile, createApp} from "vue";
+import {createApp} from "vue";
+import App from "./pages/products.vue";
 
-const template = '<h1>Hi! I ❤️ {{ firstname }}</h1>';
-
-const app = createApp({
-    data() {
-        return {
-            firstname: 'Doudou'
-        }
-    },
-    render() {
-        return compile(template)(this.$.ctx)
-    }
-});
-
-
-app.mount('#app');
+createApp(App).mount('#app');
