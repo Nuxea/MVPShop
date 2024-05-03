a725a7814f5352b49fb46074a1d8eee982c42d0a
Nuxea <lescieux.am@gmail.com>
2024-05-03 11:17:57 +0200

    Vue Instance & Dynamic Data

--- assets/products.js
+++ assets/products.js
@@ -1,7 +1,17 @@
-import {createApp} from "vue";
+import {compile, createApp} from "vue";
+
+const template = '<h1>Hi! I ❤️ {{ firstname }}</h1>';
 
 const app = createApp({
-    template: '<h1>Hi! I ❤️ Vue app here...</h1>',
+    data() {
+        return {
+            firstname: 'Doudou'
+        }
+    },
+    render() {
+        return compile(template)(this.$.ctx)
+    }
 });
+
 
 app.mount('#app');
