236ba827415f078f7c9ab00f89136db140fc489d
Nuxea <lescieux.am@gmail.com>
2024-10-21 18:17:52 +0200

    Loading Component

---
+++ assets/js/components/loading.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <h4 class="loadingImg">
+        Loading...
+    </h4>
+</template>
+
+<style scoped>
+.loadingImg {
+  background: url("../../images/loading.gif") no-repeat left center;
+  padding: 0 0 4px 50px;
+}
+</style>
--- assets/js/components/product/product-list.vue
+++ assets/js/components/product/product-list.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ProductCard from "@/components/product/product-card.vue";
+import Loading from "@/components/loading.vue";
 
 defineProps({
     products: {
@@ -11,8 +12,14 @@ defineProps({
 
 <template>
     <div class="row">
+        <div class="col-12">
+            <div class="mt-4">
+                <Loading v-show="products.length === 0" />
+            </div>
+        </div>
         <product-card
             v-for="product in products"
+            v-show="products.length > 0"
             :key="product['@id']"
             :product="product"
         />
